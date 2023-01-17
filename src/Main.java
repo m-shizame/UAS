472fb59e2cc747a5a5b454ed200e2cd5299fb38f
xiaom <muttaqienshizame9@gmail.com>
2023-01-17 09:10:19 +0700

    22090148

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+int jumlah=4;
+UASID myMessage = new UASID();
+        Scanner sc = new Scanner(System.in);
+        myMessage.setMessageID(sc.nextLine());
+        myMessage.setMessageName(sc.nextLine());
+        ArrayList<String> context = new ArrayList<>();
+        for(int index=0; index<jumlah;index++){
+          context.add(sc.nextLine());
+        }
+        myMessage.setcontext(context);
+        System.out.println("kata yang dicari :");
+        String cari = sc.nextLine();
+        System.out.println(myMessage.FindWords(context,cari));
     }
 }
--- src/UASID.java
+++ src/UASID.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class UASID {
+import java.util.ArrayList;
+
+public class UASID {
+    private static final String UASID = ;
+
+    public static void setMessageID(String nextLine) {
+    }
+
+    public void setMessageName(String nextLine) {
+    }
+
+    public void setcontext(ArrayList<String> context) {
+    }
+
+    public String FindWords(ArrayList<String> context, String cari) {
+        return UASID;
+    }
 }
